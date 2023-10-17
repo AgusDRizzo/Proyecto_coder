0e641ab45bc7131836edff9796bba58dd60bde20
AgustinDRizzo <agusdrizzo999@gmail.com>
2023-10-16 22:40:31 -0400

    Formulario de registro completo

--- cuenta/forms.py
+++ cuenta/forms.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
 
 class FormularioRegistro(UserCreationForm):
-    ...
-    
+    email = forms.EmailField()
+    password1 = forms.CharField(label = "Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label = "Repetir conraseña", widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+        help_texts = {campo:"" for campo in fields}    
--- cuenta/views.py
+++ cuenta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login as django_login
+from cuenta.forms import FormularioRegistro
 def login(request):
     if request.method == "POST":
         formulario = AuthenticationForm(request, data= request.POST)
@@ -15,12 +16,12 @@ def login(request):
         return render (request, "cuenta/login.html", {"formulario":formulario})
 def registro(request):
     if request.method == "POST":
-        formulario = AuthenticationForm(request.POST)
+        formulario = FormularioRegistro(request.POST)
         if formulario.is_valid():
             formulario.save()
             return redirect("login")
     else:
-        formulario = UserCreationForm()
+        formulario = FormularioRegistro()
         return render(request, "cuenta/registro.html",  {"formulario":formulario})
        
 # Create your views here.
