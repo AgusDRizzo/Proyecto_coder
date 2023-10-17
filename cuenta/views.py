67ab118172a46406f456c23ea7e58818317e2a0c
AgustinDRizzo <agusdrizzo999@gmail.com>
2023-10-16 21:59:58 -0400

    mitad de clase 13

---
+++ cuenta/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login as django_login
+def login(request):
+    if request.method == "POST":
+        formulario = AuthenticationForm(request, data= request.POST)
+        if formulario.is_valid():
+            username = formulario.cleaned_data.get("username")
+            password = formulario.cleaned_data.get("password")
+            usuario = authenticate(username=username, password=password)
+            django_login(request, usuario)
+            return redirect("inicio")
+    else:
+        formulario = AuthenticationForm()
+        return render (request, "cuenta/login.html", {"formulario":formulario})
+def registro(request):
+    if request.method == "POST":
+        formulario = AuthenticationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect("login")
+    else:
+        formulario = UserCreationForm()
+        return render(request, "cuenta/registro.html",  {"formulario":formulario})
+       
+# Create your views here.
